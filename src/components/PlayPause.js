a782fab8be893bba3ad4bda6a403fc6288941f45
Zia Grosvenor <ziagrosvenor@live.co.uk>
2017-03-16 01:05:28 +0000

    Add media player code

---
+++ src/components/PlayPause.js
@@ -0,0 +1,54 @@
+import React, { Component, PropTypes } from 'react'
+import Transition from 'react-motion-ui-pack'
+
+class ScaleX extends Component {
+  render() {
+    return (
+      <Transition
+        component="g"
+        enter={{ scaleX: 1 }}
+        leave={{ scaleX: 0 }}
+      >
+        {this.props.children}
+      </Transition>
+    )
+  }
+}
+
+class PlayPause extends Component {
+  render() {
+    const {  isPlaying , className } = this.props
+    return (
+      <svg
+        role="button"
+        width="36px"
+        height="36px"
+        viewBox="0 0 36 36"
+        className={className}
+        onClick={this.props.togglePlaying}
+      >
+      	<circle fill="#373D3F" cx="18" cy="18" r="18"/>
+          <ScaleX>
+            { isPlaying &&
+              <g key="pause" style={{ transformOrigin: '0% 50%' }}>
+        	      <rect x="12" y="11" fill="#CDD7DB" width="4" height="14"/>
+        	      <rect x="20" y="11" fill="#CDD7DB" width="4" height="14"/>
+              </g>
+            }
+          </ScaleX>
+          <ScaleX>
+            { !isPlaying &&
+              <polygon
+                key="play"
+                fill="#CDD7DB"
+                points="14,11 26,18 14,25"
+                style={{ transformOrigin: '100% 50%' }}
+              />
+            }
+          </ScaleX>
+      </svg>
+    )
+  }
+}
+
+export default PlayPause
